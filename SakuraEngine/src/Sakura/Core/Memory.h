d15322a847313c84e1f330c64d7660d8e1b15c31
Frollo24 <57634169+Frollo24@users.noreply.github.com>
2024-09-03 19:28:34 +0200

    Fixed memory module not working

--- SakuraEngine/src/Sakura/Core/Memory.h
+++ SakuraEngine/src/Sakura/Core/Memory.h
@@ -7,16 +7,16 @@ namespace Sakura
 	template <typename T>
 	using Unique = std::unique_ptr<T>;
 	template <typename T, typename... Args>
-	Unique<T> CreateUnique(Args&&... args)
+	constexpr Unique<T> CreateUnique(Args&&... args)
 	{
-		return std::make_unique<T>(std::forward<Args>(args));
+		return std::make_unique<T>(std::forward<Args>(args)...);
 	}
 
 	template <typename T>
 	using Ref = std::shared_ptr<T>;
 	template <typename T, typename... Args>
-	Ref<T> CreateRef(Args&&... args)
+	constexpr Ref<T> CreateRef(Args&&... args)
 	{
-		return std::make_shared<T>(std::forward<Args>(args));
+		return std::make_shared<T>(std::forward<Args>(args)...);
 	}
 }
